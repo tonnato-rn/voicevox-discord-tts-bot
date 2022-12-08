4a019ebe9256accfce05a293389c92548fcd33a9
tonnato-rn <107282436+tonnato-rn@users.noreply.github.com>
2022-12-08 16:10:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const data = {
-    token: "discord token",
-    voicevoxtoken: "voicevox token"
-}
-
-
+const keys = require("./config")
 
 process.on("uncaughtException", (err) => console.log(err))
 const discord = require('discord.js')
 const request = require("request")
 const voice = require("@discordjs/voice")
+const keyv = require("keyv")
 const client = new discord.Client(require("discordjs-allintents-v14"))
-client.login(data.token)
+client.login(keys.token)
+
+client.on("ready", async function() {
+    console.log("ready")
+    delete require.cache[require.resolve("./command")]
+    client.application.commands.set(await require("./command")())
+})
 
 client.on("messageCreate", async function (message) {
     if (message.author.bot) return
@@ -23,7 +25,7 @@ client.on("messageCreate", async function (message) {
             const player = voice.createAudioPlayer()
             connection.subscribe(player)
             const { exec } = require("child_process")
-            exec(`wget -O ${__dirname}/mp3/${data.id}.mp3 "https://api.su-shiki.com/v2/voicevox/audio?text=${data.content}&speaker=8&key=${data.voicevoxtoken}"`, async function () {
+            exec(`wget -O ${__dirname}/mp3/${data.id}.mp3 "https://api.su-shiki.com/v2/voicevox/audio?text=${data.content}&speaker=${data.voice||"3"}&key=${keys.voicevox_key}"`, async function () {
                 const audio = voice.createAudioResource(`${__dirname}/mp3/${data.id}.mp3`)
                 player.play(audio)
                 player.on("stateChange", async function (Old, New) {
@@ -34,14 +36,17 @@ client.on("messageCreate", async function (message) {
                 })
             })
         })
-        await queue.push({ id: `${message.guildId}_join`, content: "接続しました。" })
+        const guildvoicedata = new keyv({ uri: "sqlite://voicedata", table: message.guildId })
+        await queue.push({ id: `${message.guildId}_join`, content: "接続しました。", voice: await guildvoicedata.get(message.member.user.id) })
         client.on("voiceStateUpdate", async function(Old, New) {
             if (message.member.voice.channelId==Old.channelId | message.member.voice.channelId==New.channelId) {
-                if (New.channelId==null && Old.channelId!=null) queue.push({ content: `${New.member.nickname||New.member.user.username}さんが退出しました`, id: `${Old.guild.id}_${Old.member.id}_leave` })
-                if (New.channelId!=null && Old.channelId==null) queue.push({ content: `${New.member.nickname||New.member.user.username}さんが入室しました`, id: `${Old.guild.id}_${Old.member.id}_join` })
+                const voice = await guildvoicedata.get(message.member.user.id)
+                if (New.channelId==null && Old.channelId!=null) queue.push({ content: `${New.member.nickname||New.member.user.username}さんが退出しました`, id: `${Old.guild.id}_${Old.member.id}_leave`, voice: voice })
+                if (New.channelId!=null && Old.channelId==null) queue.push({ content: `${New.member.nickname||New.member.user.username}さんが入室しました`, id: `${Old.guild.id}_${Old.member.id}_join`, voice: voice })
             }
         })
         message.channel.createMessageCollector().on("collect", async function (message) {
+            const voice = await guildvoicedata.get(message.member.user.id)
             let content = message.content.replace(/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#\u3000-\u30FE\u4E00-\u9FA0\uFF01-\uFFE3]+/g,"url省略").replace(/```[\s\S]*```/g, "コード省略").replace(/<@.*>/g, "メンション").replace(/<#.*>/g, "チャンネル").replace(/<@&.*>/g, "ロール").replace(/(´・ω・｀)/g, "しょぼーん")
             if (message.attachments.map(d=>d).length!=0) {
                 content = `${content}。添付ファイル`
@@ -49,7 +54,7 @@ client.on("messageCreate", async function (message) {
             if (message.reference) {
                 content = `リプライ。${content}`
             }
-            if (!message.content.startsWith("v?")) if (!message.author.bot) queue.push({ id: message.id, content: content })
+            if (!message.content.startsWith("v?")) if (!message.author.bot) queue.push({ id: message.id, content: content, voice: voice })
         })
     } else if (message.content == "v?dc") {
         if (!message.member.voice.channel | !message.guild.members.me.voice.channel) return message.reply("ボイスチャンネルにいません。")
@@ -58,6 +63,15 @@ client.on("messageCreate", async function (message) {
     }
 })
 
-client.on("ready", async function() {
-    console.log("Ready")
+client.on("interactionCreate", async function(interaction) {
+    if (interaction.isCommand()) {
+        const cn = interaction.commandName
+        if (cn == "setvoice") {
+            const voice = interaction.options.getString("voice")
+            const guildvoicedata = new keyv({ uri: "sqlite://voicedata", table: interaction.guildId })
+            guildvoicedata.set(interaction.user.id, voice)
+            interaction.reply({content: "設定しました", ephemeral: true})
+        }
+        
+    }
 })
