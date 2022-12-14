ff58a3445e061fd8dd77d78a78b14f3bb9395cd8
tonnato-rn <107282436+tonnato-rn@users.noreply.github.com>
2022-12-14 13:51:10 +0900

    Update command.js

--- command.js
+++ command.js
@@ -3,14 +3,12 @@ const keys = require("./config")
 const request = require("async-request");
 
 module.exports = async function () {
-    const voices = await request("https://api.su-shiki.com/v2/voicevox/speakers?key="+keys.voicevox_key)
+    const voices = await request("https://tbot.tonnatorn.com/speakers")
     voices.body = JSON.parse(voices.body)
     const commands = [
         new SlashCommandBuilder()
             .setName("setvoice")
             .setDescription("Set TTS Voice")
-            .addStringOption(o => o.setName("voice").setRequired(true).setDescription("Voice").addChoices())
     ]
-    await voices.body.map(d=> commands[0].options[0].choices.push({ "name": d.name, "value": `${d.styles[0].id}` }))
     return commands
 }
